93cb214666a2fdcca43482b6ee15213e589eb290
jwjung84@gmail.com <cjswosla1>
2015-12-23 22:31:06 -0500

    .

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ __author__ = 'PocketJacks74'
 from urllib2 import Request, urlopen
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from StringIO import StringIO
+import pandas as pd
+import math
 
-url = "http://www.federalreserve.gov/monetarypolicy/files/fomcmoa19800109.pdf"
-writer = PdfFileWriter()
+df_date = pd.read_csv('FOMC_Dates.csv')
+url = "http://www.federalreserve.gov"
+df_date['NLP']=0
+for i, filename in enumerate(df_date.url):
+        if filename=='nan':
+                pass
+        elif filename[-3:] == 'pdf':
+                remoteFile = urlopen(Request(url + filename)).read()
+                memoryFile = StringIO(remoteFile)
+                pdfFile = PdfFileReader(memoryFile)
+                pagelist = []
+                for pageNum in xrange(pdfFile.getNumPages()):
+                        currentPage = pdfFile.getPage(pageNum)
+                        myText = currentPage.extractText()
+                        thispage = myText.split()
+                        pagelist = pagelist + thispage
+                df_date['NLP'].iloc[i] = pagelist
 
-remoteFile = urlopen(Request(url)).read()
-memoryFile = StringIO(remoteFile)
-pdfFile = PdfFileReader(memoryFile)
 
-# initialize page list
 
-pagelist = []
-for pageNum in xrange(pdfFile.getNumPages()):
-        currentPage = pdfFile.getPage(pageNum)
-        myText = currentPage.extractText()
-        thispage = myText.split()
-        pagelist = pagelist + thispage
-
-print len(pagelist)
