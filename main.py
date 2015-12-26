44daebd98d93c515a9ff5c5145db42f44c8d9fbf
jwjung84@gmail.com <cjswosla1>
2015-12-26 07:10:05 -0500

    .

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from urllib2 import Request, urlopen
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from StringIO import StringIO
 import pandas as pd
+from bs4 import BeautifulSoup
+import math
 import re
 
-df_date = pd.read_csv('FOMC_Dates.csv')
+df_date = pd.read_csv('FOMC_Dates_modified.csv')
 url = "http://www.federalreserve.gov"
 df_date['NLP']=0
-pattern='<P>(.*)</P>'
+#pattern_list=['<P>(.*)</P>','<P>(.*)','<p>(.*)</p>','<p>(.*)']
 
 for i, filename in enumerate(df_date.url):
-        if filename=='nan':
-                pass
+        if pd.isnull(filename):
+                print str(i) + 'th record is not availale'
         elif filename[-3:] == 'pdf':
                 remoteFile = urlopen(Request(url + filename)).read()
                 memoryFile = StringIO(remoteFile)
@@ -25,14 +27,19 @@ for i, filename in enumerate(df_date.url):
                         thispage = myText.split()
                         pagelist = pagelist + thispage
                 df_date['NLP'].iloc[i] = pagelist
-
-        elif filename[-3:] == 'htm':
-                remoteFile = urlopen(Request(url + filename)).read()
-                m=re.findall(pattern,remoteFile)
+        else:
+                if filename[:4] == 'http':
+                        remoteFile = BeautifulSoup(urlopen(Request(filename)).read())
+                else:
+                        remoteFile = BeautifulSoup(urlopen(Request(url + filename)).read())
+                #print filename
                 pagelist = []
+                m=remoteFile.find_all('p')
                 for m_sub in m:
-                        pagelist = pagelist + m_sub.split()
+                        pagelist = pagelist + m_sub.get_text().split()
                 df_date['NLP'].iloc[i] = pagelist
+                #print pagelist
+
 
 
 
